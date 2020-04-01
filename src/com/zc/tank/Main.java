4bf66475b9f82096dfd3c11a4c76365646168fba
hughzc <hughzcheng@163.com>
2020-04-01 15:35:19 +0800

    20200401

---
+++ src/com/zc/tank/Main.java
@@ -0,0 +1,12 @@
+package com.zc.tank;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        TankFrame f = new TankFrame();
+
+        while (true){
+            Thread.sleep(50);
+            f.repaint();
+        }
+    }
+}
